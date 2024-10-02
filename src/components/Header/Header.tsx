9e5ab2c740ea996de57f03574113c8ff06587fb0
Vperewalow <perewalow@gmail.com>
2024-10-02 23:20:34 +0300

    bugs fixed

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import './Header.css';
+
+function Header({ inputText, setInputText, addTodo, deleteAllTodo, loadTodo, deleteLastTodo, searchTodo }:
+    {
+        inputText: string,
+        setInputText: (value: string) => void,
+        addTodo: () => void,
+        deleteAllTodo: () => void,
+        loadTodo: () => void,
+        deleteLastTodo: () => void,
+        searchTodo: (query: string) => void
+    }) {
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        searchTodo(e.target.value);
+    };
+    return (
+        <div className='header'>
+            <button className='header__button' onClick={deleteAllTodo}>Delete All</button>
+            <button className='header__button' onClick={deleteLastTodo}>Delete Last</button>
+            <input placeholder='Add todo'
+                className='header__input'
+                value={inputText}
+                onChange={(e) => setInputText(e.target.value)}>
+            </input>
+            <button className='header__button' onClick={addTodo}>Add</button>
+            <button className='header__button' onClick={loadTodo}>Load</button>
+        </div>
+    );
+}
+
+export default Header;
